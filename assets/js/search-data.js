3aef7710cba512987818bbfd059c948564d545dd
anaismoller <anaismoller@gmail.com>
2025-09-12 00:11:02 +0000

    Deploying to gh-pages from @ anaismoller/anaismoller.github.io@1a0984b13d7c2c9f6c3cc45728d1b708d0009430 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
